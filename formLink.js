6d4cf69da937be3db72eb90b530f6b9f604e8d3f
KowshikrajK <kowshikraj077@gmail.com>
2024-01-27 20:11:11 +0530

    sc

--- formLink.js
+++ formLink.js
@@ -1,3 +1,34 @@
+const menuBtn = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navLeft');
+const closeBtn = document.querySelector('#close-icon');
+
+if(menuBtn){
+    menuBtn.addEventListener('click', function() {
+        closeBtn.style.display = 'block';
+        menuBtn.style.display = 'none';
+        
+        if (navbar.style.display === 'flex') {
+
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'flex';
+        }
+    });
+}
+
+if(closeBtn){
+    closeBtn.addEventListener('click', function() {
+        closeBtn.style.display = 'none';
+        menuBtn.style.display = 'block';
+        
+        if (navbar.style.display === 'flex') {
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'flex';
+        }
+    });
+}
+
 const formLinkBtns = document.querySelectorAll('#formLink');
 
 if (formLinkBtns) {
