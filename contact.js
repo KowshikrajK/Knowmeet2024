c5879af0053056bf924c7829b9ec4efbd7c0b539
KowshikrajK <kowshikraj077@gmail.com>
2024-01-27 20:01:09 +0530

    sc

--- contact.js
+++ contact.js
@@ -1,3 +1,33 @@
+const menuBtn = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navLeft');
+const closeBtn = document.querySelector('#close-icon');
+
+if(menuBtn){
+    menuBtn.addEventListener('click', function() {
+        closeBtn.style.display = 'block';
+        menuBtn.style.display = 'none';
+        
+        if (navbar.style.display === 'flex') {
+
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'flex';
+        }
+    });
+}
+
+if(closeBtn){
+    closeBtn.addEventListener('click', function() {
+        closeBtn.style.display = 'none';
+        menuBtn.style.display = 'block';
+        
+        if (navbar.style.display === 'flex') {
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'flex';
+        }
+    });
+}
 
 const linkTreeBtn = document.querySelector('#linkTree');
 if(linkTreeBtn){
